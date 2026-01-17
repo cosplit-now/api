0b94d0eee37203914d015f2b68cbab054dbcc338
Xinqi Mu <hi@muxinqi.com>
2026-01-17 17:26:54 +0000

    refactor: standardize quote style to double quotes across the codebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from '@thallesp/nestjs-better-auth';
-import { auth } from './lib/auth';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "@thallesp/nestjs-better-auth";
+import { auth } from "./lib/auth";
 
 @Module({
   imports: [AuthModule.forRoot({ auth })],
