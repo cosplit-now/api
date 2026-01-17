078d2ed3d9384822933e4f8733bb32608c3cb53e
Xinqi Mu <hi@muxinqi.com>
2026-01-17 23:13:53 +0000

    feat(cors): enable credentials for CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bodyParser: false,
   });
-  app.enableCors({ origin: ["http://localhost:5173"] });
+  app.enableCors({ origin: ["http://localhost:5173"], credentials: true });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
